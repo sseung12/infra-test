b8a22538243827b22572f7f75269effbdb6efa2c
sseung12 <pon02129@naver.com>
2022-04-13 17:13:36 +0900

    init

---
+++ src/main/java/com/aws/infratest/InfraTestApplication.java
@@ -0,0 +1,13 @@
+package com.aws.infratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/infratest/InfraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.infratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
